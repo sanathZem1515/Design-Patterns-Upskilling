e095863ffd0dc054c1b8361007904fd123e2444d
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-02 22:09:11 +0530

    Singleton Fixes

--- Singleton-ThreadPool/src/Main.java
+++ Singleton-ThreadPool/src/Main.java
@@ -2,9 +2,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        ThreadPool t = new ThreadPool();
-        t.getInstance2();
-
+        ThreadPool t = ThreadPool.getInstance2();
+        
         Thread t1 = new Thread(new Task("A"));
         Thread t2 = new Thread(new Task("B"));
         Thread t3 = new Thread(new Task("C"));
--- Singleton-ThreadPool/src/ThreadPool.java
+++ Singleton-ThreadPool/src/ThreadPool.java
@@ -1,13 +1,13 @@
 public class ThreadPool {
 
     private static int size = 10;
-    private volatile ThreadPool threadPoolObject = null;
+    private static volatile ThreadPool threadPoolObject = null;
     public static volatile int count = size;
 
     ThreadPool() {
     }
 
-    public ThreadPool getInstance2() {
+    public static ThreadPool getInstance2() {
         if (threadPoolObject == null) {
             synchronized (ThreadPool.class) {
                 if (threadPoolObject == null) {
