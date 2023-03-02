278669d814511031d0f6385fd1237921b4c0718c
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-02 23:17:30 +0530

    removed single ton files

--- Singleton-ThreadPool/src/Main.java
+++ Singleton-ThreadPool/src/Main.java
@@ -1,20 +1,5 @@
-
 public class Main {
-    public static void main(String[] args) {
-
-        ThreadPool t = ThreadPool.getInstance2();
-        
-        Thread t1 = new Thread(new Task("A"));
-        Thread t2 = new Thread(new Task("B"));
-        Thread t3 = new Thread(new Task("C"));
-        Thread t4 = new Thread(new Task("D"));
-        Thread t5 = new Thread(new Task("E"));
-
-        t.execute(t1);
-        t.execute(t2);
-        t.execute(t3);
-        t.execute(t4);
-        t.execute(t5);
-
+    public  static void main(String args[]) {
+        System.out.println("Hello World");
     }
 }
